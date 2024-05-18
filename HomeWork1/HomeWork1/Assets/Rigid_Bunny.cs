bbaf8a2b1db793ccbf38c6e2af75fca7fa5003d5
xxd <17826833461@163.com>
2024-05-18 16:09:22 +0800

    try collision

--- HomeWork1/HomeWork1/Assets/Rigid_Bunny.cs
+++ HomeWork1/HomeWork1/Assets/Rigid_Bunny.cs
@@ -56,6 +56,65 @@ public class Rigid_Bunny : MonoBehaviour
 	//a plane <P, N>
 	void Collision_Impulse(Vector3 P, Vector3 N)
 	{
+		var R = Matrix4x4.Rotate(transform.rotation);
+		Vector3 pos = transform.position;
+		
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		for (int i=0; i<vertices.Length; i++)
+		{
+			var Rri = R * vertices[0];
+			var vecticePos = pos + (Vector3)Rri;
+			
+			//check the vertice is in plane
+			if (Vector3.Dot(vecticePos - P, N) >= 0)
+				continue;
+			
+			//check the velocity is directed in plane
+			var tmpV = v + Vector3.Cross(w, Rri);
+			if (Vector3.Dot(tmpV, N) >= 0)
+				continue;
+			
+			//calculate vertice's new velocity
+			var vin = Vector3.Dot(tmpV, N) * N;
+			var vit = tmpV - vin;
+			var vinNew = -restitution * vin;
+			var vitNew = Mathf.Max(1 - 0.5f * (1 + restitution) * Mathf.Abs(vin.magnitude) / Mathf.Abs(vit.magnitude), 0) * vit;
+			var viNew = vinNew + vitNew;
+			
+			//calculate Impulse
+			var I = R * I_ref * R.transpose;
+			var Rris = Get_Cross_Matrix(Rri);
+			var kTmp = Rris * I.inverse * Rris;
+			Matrix4x4 k = Matrix4x4.zero;
+			k [0, 0] = 1 / mass - kTmp[0, 0]; 
+			k [0, 1] = - kTmp[0, 1]; 
+			k [0, 2] = - kTmp[0, 2]; 
+			k [0, 3] = - kTmp[0, 3]; 
+			
+			k [1, 0] = - kTmp[1, 0]; 
+			k [1, 1] = 1 / mass - kTmp[1, 1]; 
+			k [1, 2] = - kTmp[1, 2]; 
+			k [1, 3] = - kTmp[1, 3]; 
+			
+			k [2, 0] = - kTmp[2, 0]; 
+			k [2, 1] = - kTmp[2, 1]; 
+			k [2, 2] = 1 / mass - kTmp[2, 2]; 
+			k [2, 3] = - kTmp[2, 3]; 
+			
+			k [3, 0] = - kTmp[3, 0];
+			k [3, 1] = - kTmp[3, 1];
+			k [3, 2] = - kTmp[3, 2];
+			k [3, 3] = 1 / mass - kTmp[3, 3];
+			
+			var j = k.inverse * (viNew - tmpV);
+
+			Vector3 vtmp = (1 / mass * j);
+			v = v + vtmp;
+			Vector3 wtmp = I.inverse * (Rris * j);
+			w = w + wtmp;
+			break;
+		}
 	}
 	
 	bool launched 		= false;
@@ -85,22 +144,27 @@ public class Rigid_Bunny : MonoBehaviour
 		if(Input.GetKey("l"))
 		{
 			v = new Vector3 (5, 2, 0);
-			w = new Vector3(0, 1000, 0);
+			w = new Vector3(0, 1, 0);
 			launched=true;
 		}
 
-		timeTotal += Time.deltaTime;
-
-		if (timeTotal / dt <= 0)
+		if (launched == false)
 			return;
-		
-		int updateTimes = Mathf.FloorToInt(timeTotal / dt);
+
+		// timeTotal += Time.deltaTime;
+		//
+		// if (timeTotal / dt <= 0)
+		// 	return;
+
+		int updateTimes = 1;//Mathf.FloorToInt(timeTotal / dt);
 		timeTotal = timeTotal % dt;
 
 		for (int i = 0; i < updateTimes; i++)
 		{
 			// Part I: Update velocities
-
+			v = v + dt * g;
+			v = linear_decay * v;
+			w = angular_decay * w;
 
 			// Part II: Collision Impulse
 			Collision_Impulse(new Vector3(0, 0.01f, 0), new Vector3(0, 1, 0));
@@ -109,17 +173,11 @@ public class Rigid_Bunny : MonoBehaviour
 			// Part III: Update position & orientation
 			//Update linear status
 			Vector3 x    = transform.position;
+			x = x + v * dt;
 			//Update angular status
 			Quaternion q = transform.rotation;
-
-			if (launched)
-			{
-				v = v + Time.deltaTime * g;
-				x = x + v * Time.deltaTime;
-				q = Quaternion.Euler(q.eulerAngles + w * Time.deltaTime);
-				w = angular_decay * w;
-				v = linear_decay * v;
-			}
+			
+			q = Quaternion.Euler(q.eulerAngles + w * dt);
 
 			// Part IV: Assign to the object
 			transform.position = x;
